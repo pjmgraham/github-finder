4e629c7e810cea22aa375ffe232dafb2df44c841
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-08-31 16:42:49 +0200

    updates .env

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const UserItem = ({ user: { login, avatar_url } }) => {
+  return <div className="card shadow-md compact side bg-base-100">{login}</div>;
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
