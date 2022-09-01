8894d92c0cc4e9f39fa0eeee56df91fc7b417db1
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-01 14:12:54 +0200

    adds search form component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 
 const Home = () => {
   return (
     <>
-      {/* search goes here */}
+      <UserSearch />
       <UserResults />
     </>
   );
